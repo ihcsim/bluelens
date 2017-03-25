f96c5804b96f4abd3de41818e4edd697196c9fee
Ivan Sim <ihcsim@gmail.com>
2017-03-25 10:12:21 -0700

    Do not error out if 'help' flag is provided.

--- cmd/blued/main.go
+++ cmd/blued/main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	"github.com/goadesign/goa"
@@ -16,6 +17,10 @@ import (
 func main() {
 	config, err := parseFlags(os.Args[1:])
 	if err != nil {
+		if err == flag.ErrHelp {
+			os.Exit(0)
+		}
+
 		log.WithFields(log.Fields{
 			"Cause": err},
 		).Fatal("Error while parsing runtime configuration flags")
